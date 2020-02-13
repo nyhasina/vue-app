ef20ff95c5f23dfc1a1f08ec16a6288cdca61fb0
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-02-13 15:37:06 +0300

    [SHARED] Add submission logic

--- src/authentication/components/sign-in-form.component.vue
+++ src/authentication/components/sign-in-form.component.vue
@@ -1,15 +1,28 @@
 <template>
     <form>
-        <form-field type="email" placeholder="E-mail address"></form-field>
-        <form-field type="password" placeholder="Password"></form-field>
-        <button>Sign in</button>
+        <form-field type="email" placeholder="E-mail address" v-model="email"></form-field>
+        <form-field type="password" placeholder="Password" v-model="password"></form-field>
+        <button @click.prevent="submit">Sign in</button>
         <router-link to="./sign-up">Have already an account ?</router-link>
     </form>
 </template>
 
 <script>
     export default {
-        name: 'AuthenticationForm'
+        name: 'AuthenticationForm',
+        data: () => ({
+            email: '',
+            password: ''
+        }),
+        methods: {
+            submit: function () {
+                const payload = {
+                    email: this.$data.email,
+                    password: this.$data.password
+                };
+                this.$emit('sign-in', payload);
+            }
+        }
     };
 </script>
 
