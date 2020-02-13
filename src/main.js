ed6c736b33aca439a12480f7f4f4417587db8dd5
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-02-13 14:53:37 +0300

    [APP] Dynamic component registering

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { camelCase, upperFirst } from 'lodash';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './app';
@@ -6,7 +7,31 @@ import { coreRoutingModule } from './core/core-routing.module';
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
+const requireComponent = require.context(
+    './shared/components',
+    false,
+    /(.)+\.component\.(.)+/
+);
+
+requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName);
+    const componentName = upperFirst(
+        camelCase(
+            // Gets the file name regardless of folder depth
+            fileName
+                .split('/')
+                .pop()
+                .replace(/(\.\w+){2}$/, '')
+        )
+    );
+    Vue.component(
+        componentName,
+        componentConfig.default || componentConfig
+    );
+});
+
 new Vue({
-  router: coreRoutingModule,
-  render: h => h(App)
+    router: coreRoutingModule,
+    render: h => h(App)
 }).$mount('#app');
+
