4a7c6df4045b100e308508007ae697a83d336880
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-02-13 17:56:22 +0300

    [SHARED] Add promise handler

--- src/authentication/store/authentication.actions.js
+++ src/authentication/store/authentication.actions.js
@@ -1,8 +1,20 @@
 import AuthenticationActionTypes from './authentication.action-types';
 import AuthenticationMutationTypes from './authentication.mutation-types';
 
+const axios = require('axios').default;
+
 export default {
     [AuthenticationActionTypes.SIGN_IN]({ commit }, payload) {
         commit(AuthenticationMutationTypes.SIGN_IN, payload);
-    },
+        axios.post('https://reqres.in/api/login', payload)
+            .then((response) => {
+                const data = {
+                    user: payload,
+                    token: response.data.token
+                };
+                commit(AuthenticationMutationTypes.SIGN_IN_SUCCESS, data);
+            })
+            .catch(error => commit(AuthenticationMutationTypes.SIGN_IN_FAILED, error));
+
+    }
 };
